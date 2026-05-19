c63e09657002f93bc4995cb89a2429882a9e3c9b
Pendy02o8 <fashion79613@gmail.com>
2026-05-19 15:31:07 +0800

    remove READ_ONLY on Employee_id.

--- src/main/java/com/pendy/cinema_scheduler/entity/Employee.java
+++ src/main/java/com/pendy/cinema_scheduler/entity/Employee.java
@@ -15,7 +15,6 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     private String name;
